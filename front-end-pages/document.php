ac9f62392902a58aabefb80411db2e4c9ef5dbbd
Vik Borges <v1k@protonmail.com>
2021-12-23 22:02:21 +0000

    updated divider

--- front-end-pages/document.php
+++ front-end-pages/document.php
@@ -2,7 +2,7 @@
 
 ?>
 
-<!-- This example requires Tailwind CSS v2.0+ -->
+<!-- divider with document name -->
 <div class="relative">
     <div class="absolute inset-0 flex items-center" aria-hidden="true">
         <div class="w-full border-t border-gray-300"></div>
