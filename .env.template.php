a1b0b2cb2489bb1ac5bd652c8ebf49843e3a5989
Vik Borges <v1k@protonmail.com>
2021-12-26 20:47:57 +0000

    added sentry

--- .env.template.php
+++ .env.template.php
@@ -7,4 +7,6 @@ define('SESSION_PREFIX', 'light-packaging-project');
 define('SESSION_USER_UID_KEY', SESSION_PREFIX . '-user-uid');
 ini_set('session.name', 'FEATHERSESSID');
 
+define('SENTRY_DSN', '');
+
 ?>
