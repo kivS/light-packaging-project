5095f9173e0a019acb098c4f1d1c9166832126aa
Vik Borges <v1k@protonmail.com>
2022-01-04 11:48:31 +0000

    updated php .env templates

--- .env.template.php
+++ .env.template.php
@@ -1,12 +1,28 @@
-<?php 
+<?php
 
+error_reporting(E_ALL);
+
+define('SITE_URL', '');
 define('DASHBOARD_URL', '');
 
+define('UPLOADS_DIR', __DIR__ . '/uploads');
 
-define('SESSION_PREFIX', 'light-packaging-project');
-define('SESSION_USER_UID_KEY', SESSION_PREFIX . '-user-uid');
-ini_set('session.name', 'FEATHERSESSID');
+define('DB_FILE', __DIR__ . '/db.sqlite3');
 
 define('SENTRY_DSN', '');
+
+define('TELEGRAM_BOT_TOKEN', '');
+define('TELEGRAM_MY_CHAT_ID', '');
+
+define('SESSION_PREFIX', '');
+define('SESSION_USER_UID_KEY', SESSION_PREFIX . '-user-uid');
+ini_set('session.name', 'FEATHERSESSID');
+ini_set('session.cookie_lifetime', 16070400);  // ~ 6 months
+ini_set('session.gc_maxlifetime', 16070400);  // ~ 6 months
+
+define('MAILGUN_API_ENDPOINT', 'https://api.mailgun.net');
+define('MAILGUN_DOMAIN', '');
+define('MAILGUN_API_KEY', '');
+define('MAILGUN_FROM', 'Project Light Packaging <project-light-packaging@' . MAILGUN_DOMAIN . '>');
 
 ?>
